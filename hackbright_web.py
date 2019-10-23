0de2a28eda549f3dcc82a432bd8ad2a62030dab9
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-23 16:58:30 -0700

    Add templates; student creation form; grade listing for student

--- hackbright_web.py
+++ hackbright_web.py
@@ -1,21 +1,63 @@
 """A web application for tracking projects, students, and student grades."""
 
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session
 
 import hackbright
 
 app = Flask(__name__)
-
+app.secret_key = "SEEKRIT"
 
 @app.route("/student")
 def get_student():
     """Show information about a student."""
 
-    github = "jhacks"
+    if request.args.get('github'):
+        github = request.args.get('github')
+    else:
+        github = session['recent_student_github']
 
     first, last, github = hackbright.get_student_by_github(github)
 
-    return "{} is the GitHub account for {} {}".format(github, first, last)
+    get_grade = hackbright.get_grades_by_github(github)
+
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github,
+                           get_grade=get_grade)
+
+    return html
+
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/new-student")
+def new_student():
+    """Display new student addition form"""
+
+    return render_template("add_student.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    session['recent_student_github'] = github
+
+    return render_template("student_added.html", first=first_name, last=last_name)
+
+
+
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
